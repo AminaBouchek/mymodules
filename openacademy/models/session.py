a6e8f09ee4389b5f3772a85a346e88d56a4a9628
amina <amina.bouchekhchoukh@gmail.com>
2022-06-08 15:02:38 +0100

    add calendar & graph views, add decoration to form view

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import timedelta
 
 
 class session(models.Model):
@@ -12,6 +13,8 @@ class session(models.Model):
     course_id = fields.Many2one('openacademy.cours', string="Course", ondelete='cascade', index=True)
     attendees_ids = fields.Many2many("res.partner", string="Attendees")
     percentage = fields.Float(compute="_compute_percentage")
+    end_date = fields.Date(string="End date", store=True, compute="_get_end_date", inverse='_set_end_date')
+    attendees_count = fields.Integer(string="Attendees count", compute="_count_attendees", store=True)
 
     @api.depends('seats', 'attendees_ids')
     def _compute_percentage(self):
@@ -20,7 +23,6 @@ class session(models.Model):
                 record.percentage = 0
             else:
                 record.percentage = len(record.attendees_ids) * 100 / record.seats
-        return record.percentage
 
     @api.onchange('seats', 'attendees.ids')
     def _verify_valid_seats(self):
@@ -34,7 +36,28 @@ class session(models.Model):
         if self.seats < len(self.attendees_ids):
             return {
                 'warning': {
-                            'title': "Too many attendees!",
-                            'message': "Send some attendees home"
-                            },
+                    'title': "Too many attendees!",
+                    'message': "Send some attendees home"
+                },
             }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+                continue
+            duration = timedelta(days=record.duration, seconds=-1)
+            record.end_date = record.start_date + duration
+
+    def _set_end_date(self):
+        for record in self:
+            if not (record.start_date and record.end_date):
+                continue
+            record.duration = (record.end_date - record.start_date).days + 1
+
+    @api.depends('attendees_ids')
+    def _count_attendees(self):
+        for record in self:
+            record.attendees_count = len(record.attendees_ids)
+
